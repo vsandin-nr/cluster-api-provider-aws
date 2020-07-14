52cacec0ee13c842711846580603604cbaecbff5
Nicole Yson <nyson@newrelic.com>
2020-07-14 10:10:12 -0700

    Adds EKSConfig reconciliation integration tests (#16)

---
+++ test/helpers/envtest.go
@@ -0,0 +1,118 @@
+/*
+Copyright 2020 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package helpers
+
+import (
+	"path"
+	"path/filepath"
+	goruntime "runtime"
+
+	"github.com/onsi/ginkgo"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
+	"k8s.io/klog"
+	"k8s.io/klog/klogr"
+
+	bootstrapv1 "sigs.k8s.io/cluster-api-provider-aws/bootstrap/eks/api/v1alpha3"
+	clusterv1 "sigs.k8s.io/cluster-api/api/v1alpha3"
+	"sigs.k8s.io/cluster-api/util"
+	"sigs.k8s.io/cluster-api/util/kubeconfig"
+
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+)
+
+func init() {
+	klog.InitFlags(nil)
+	ctrl.SetLogger(klogr.New())
+	klog.SetOutput(ginkgo.GinkgoWriter)
+}
+
+var (
+	env *envtest.Environment
+)
+
+func init() {
+	// Calculate the scheme.
+	utilruntime.Must(clusterv1.AddToScheme(scheme.Scheme))
+	utilruntime.Must(bootstrapv1.AddToScheme(scheme.Scheme))
+
+	// Get the root of the current file to use in CRD paths.
+	_, filename, _, _ := goruntime.Caller(0) //nolint
+	root := path.Join(path.Dir(filename), "..", "..")
+
+	// Create the test environment.
+	env = &envtest.Environment{
+		ErrorIfCRDPathMissing: true,
+		CRDDirectoryPaths: []string{
+			filepath.Join(root, "config", "crd", "bases"),
+			filepath.Join(root, "bootstrap", "eks", "config", "crd", "bases"),
+		},
+	}
+}
+
+// TestEnvironment encapsulates a Kubernetes local test environment.
+type TestEnvironment struct {
+	manager.Manager
+	client.Client
+	Config *rest.Config
+
+	doneMgr chan struct{}
+}
+
+// NewTestEnvironment creates a new environment spinning up a local api-server.
+//
+// This function should be called only once for each package you're running tests within,
+// usually the environment is initialized in a suite_test.go file within a `BeforeSuite` ginkgo block.
+func NewTestEnvironment() *TestEnvironment {
+	if _, err := env.Start(); err != nil {
+		panic(err)
+	}
+
+	mgr, err := manager.New(env.Config, manager.Options{
+		Scheme:             scheme.Scheme,
+		MetricsBindAddress: "0",
+		NewClient:          util.ManagerDelegatingClientFunc,
+	})
+	if err != nil {
+		klog.Fatalf("Failed to start testenv manager: %v", err)
+	}
+
+	return &TestEnvironment{
+		Manager: mgr,
+		Client:  mgr.GetClient(),
+		Config:  mgr.GetConfig(),
+		doneMgr: make(chan struct{}),
+	}
+}
+
+func (t *TestEnvironment) StartManager() error {
+	return t.Manager.Start(t.doneMgr)
+}
+
+func (t *TestEnvironment) Stop() error {
+	t.doneMgr <- struct{}{}
+	return env.Stop()
+}
+
+// TODO(rudoi): is this required? how will this work with EKS?
+func (t *TestEnvironment) CreateKubeconfigSecret(cluster *clusterv1.Cluster) error {
+	return kubeconfig.CreateEnvTestSecret(t.Client, t.Config, cluster)
+}
