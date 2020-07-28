fb3c0e704d2a25280a5fc051aa03dc7c0ca6bcb4
mnguyen <mnguyen@newrelic.com>
2020-07-28 10:35:01 -0700

    wip

--- pkg/cloud/services/autoscaling/autoscalinggroup.go
+++ pkg/cloud/services/autoscaling/autoscalinggroup.go
@@ -22,6 +22,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/autoscaling"
 	"github.com/pkg/errors"
+	"k8s.io/utils/pointer"
 	infrav1 "sigs.k8s.io/cluster-api-provider-aws/api/v1alpha3"
 	expinfrav1 "sigs.k8s.io/cluster-api-provider-aws/exp/api/v1alpha3"
 	"sigs.k8s.io/cluster-api-provider-aws/pkg/cloud/awserrors"
@@ -178,7 +179,7 @@ func (s *Service) CreateASG(scope *scope.MachinePoolScope) (*expinfrav1.AutoScal
 func (s *Service) runPool(i *expinfrav1.AutoScalingGroup) (*expinfrav1.AutoScalingGroup, error) {
 	input := &autoscaling.CreateAutoScalingGroupInput{
 		AutoScalingGroupName: aws.String(i.Name),
-		DesiredCapacity:      aws.Int64(int64(i.DesiredCapacity)),
+		DesiredCapacity:      aws.Int64(int64(pointer.Int32PtrDerefOr(i.DesiredCapacity, 0))),
 		MaxSize:              aws.Int64(int64(i.MaxSize)),
 		MinSize:              aws.Int64(int64(i.MinSize)),
 		VPCZoneIdentifier:    aws.String(strings.Join(i.Subnets, ", ")),
