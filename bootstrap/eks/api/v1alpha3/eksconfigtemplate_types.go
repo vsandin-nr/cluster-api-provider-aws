25e7a8e4183ee87665e7c194974e838774aa01f0
Andrew Rudoi <arudoi@newrelic.com>
2020-08-12 09:04:53 -0700

    fix: appease the linter

--- bootstrap/eks/api/v1alpha3/eksconfigtemplate_types.go
+++ bootstrap/eks/api/v1alpha3/eksconfigtemplate_types.go
@@ -20,7 +20,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-
 // EKSConfigTemplateSpec defines the desired state of EKSConfigTemplate
 type EKSConfigTemplateSpec struct {
 	Template EKSConfigTemplateResource `json:"template"`
